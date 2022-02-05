cd54399be68305f5f49520842043b79090e32949
Duarte Cancela <duartecancela@gmail.com>
2022-02-05 17:12:31 +0000

    add average

---
+++ GSA_DBF/Areas/Alunos/Controllers/ClassificacaoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GSA_DBF.Models;
+
+namespace GSA_DBF.Areas.Alunos.Controllers
+{
+    public class ClassificacaoController : Controller
+    {
+        private gsa_dbaEntities1 db = new gsa_dbaEntities1();
+
+        // GET: Alunos/Classificacao
+        public ActionResult Index()
+        {
+            var classificacao = db.Classificacao.Include(c => c.Aluno).Include(c => c.Epoca).Include(c => c.UC);
+            return View(classificacao.ToList());
+        }
+
+        public ActionResult Average()
+        {
+            var classificacao = db.Classificacao.Where(c => c.id_uc == 1);
+            return View(classificacao.ToList());
+        }
+    }
+}
